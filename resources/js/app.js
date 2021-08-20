a4241ae240e90477783e0be11940cee87ba50f6f
Oscar Ruiz <oscaralexruiz@gmail.com>
2021-08-19 19:20:34 -0500

    configuration vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+window.Vue = require('vue').default;
+
+import App from './components/App.vue';
+import vuex from './plugins/vuex/store'
+import vuetify from './plugins/vuetify/vuetify';
+
+new Vue({
+    render : render=>render('App'),
+    components : {
+        App
+    },
+    vuex,
+    vuetify
+}).$mount('#app');
